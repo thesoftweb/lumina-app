f1e9495a75ded2e60e39e7df5576c12066086f49
Matthaus Ceci <matthausnawan@gmail.com>
2025-12-01 13:48:11 -0300

    feat:supplier-form

---
+++ app/Filament/Resources/Suppliers/Schemas/SupplierForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources\Suppliers\Schemas;
+
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Schema;
+
+class SupplierForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make('Informações de Fornecedor')
+                    ->columnSpanFull()
+                    ->columns(2)
+                    ->schema([
+                        Radio::make('document_type')
+                            ->columnSpanFull()
+                            ->label('Tipo de Cadastro')
+                            ->required()
+                            ->reactive()
+                            ->options([
+                                'F' => 'Pessoa Física',
+                                'J' => 'Pessoa Jurídica'
+                            ])
+                            ->default('F')
+                            ->inline(),
+                        TextInput::make('document')
+                            ->prefixIcon('heroicon-o-identification')
+                            ->label(fn (Get $get): string => $get('document_type') === 'F' ? 'CPF' : 'CNPJ')
+                            ->required(),
+                        TextInput::make('name')
+                            ->label('Nome')
+                            ->required(),
+                        TextInput::make('description')
+                            ->label('Descrição')
+                            ->required(),
+                        TextInput::make('email')
+                            ->label('E-mail'),
+                        TextInput::make('phone')
+                            ->label('Contato'),
+                        Textarea::make('details')
+                            ->columnSpanFull()
+                            ->label('Observações')
+                    ])
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Suppliers/Tables/SuppliersTable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\Suppliers\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SuppliersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->description(fn ($record) => $record->description)
+                    ->label('Nome')
+                    ->searchable(),
+                TextColumn::make('phone')
+                    ->label('Contato')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('E-mail')
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
