7f310d76bf53e9a0a5c43e097ccbfd5889ed6219
Matthaus Nawan <matthausnawan@gmail.com>
2025-12-01 11:19:40 -0300

    feat:checklist

---
+++ database/migrations/2025_11_28_212325_create_checklist_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checklist_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('checklist_id')->constrained('checklists')->onDelete('cascade');
+            $table->foreignId('enrollment_id')->constrained('enrollments')->onDelete('cascade');
+            $table->string('item_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checklist_items');
+    }
+};
